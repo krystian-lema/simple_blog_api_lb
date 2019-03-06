4854dbfdf787734b4ba2cbf8547f8d4857532d53
Krystian Lema <krystian.lema@gmail.com>
2019-03-06 01:28:10 +0100

    Add include all method to article model.

--- server/models/article.js
+++ server/models/article.js
@@ -2,4 +2,37 @@
 
 module.exports = function(Article) {
 
+    /**
+     * Include all data from other models.
+     * @param {Function(Error, object)} callback
+     */
+    Article.include_all_data = function(callback) {
+        var result = [];
+        var filter = {
+            // fields: ['id', 'title', 'content', 'author'],
+            include: ['author', 'comments','tags']
+        };
+
+        Article.find(filter, function(err, articles) {
+            if (err !== null) {
+                return callback(err);
+            }
+
+            if (articles.length) {
+                articles.forEach(function(article) {
+                    result.push(article);
+                });
+            }
+
+            if (result.length) {
+                callback(null, result);
+            }
+            else {
+                callback(null, []);
+            }
+        });
+    };
+
+    // next methods here
+
 };
